b7449c980bb25193792cc0abee73562d044ff65d
David Golden <dagolden@cpan.org>
2011-10-07 14:04:06 -0400

    whitespace cleanup

--- ubuntu-bootstrap-iso/ubuntu-bootstrap-iso.pl
+++ ubuntu-bootstrap-iso/ubuntu-bootstrap-iso.pl
@@ -194,6 +194,6 @@ not be cleaned up.
 =head1 COPYRIGHT AND LICENSE
 
 This software is Copyright (c) 2011 by David Golden.
- 
+
 This is free software, licensed under The Apache License, Version 2.0, January
 2004.
